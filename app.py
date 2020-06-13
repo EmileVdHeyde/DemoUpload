cac43f1c5487c64cc12c475d8dfcbea8ac33743d
Emile Van Der Heyde <emilevdheyde@gmail.com>
2020-06-13 22:25:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def upload():
         file.save(destination)
         output= file.read()
 
-    return render_template("complete.html", output=print(output))
+    return render_template("complete.html", output=output)
     
 if __name__ == "__main__":
     app.run(port=4555, debug=True)
